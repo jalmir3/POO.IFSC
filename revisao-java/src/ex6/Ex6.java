d21a42663f8b09d459b4a05f47a141e3da5409b4
Jalmir Winter <104005909+jalmir3@users.noreply.github.com>
2022-08-18 19:06:52 -0300

    Update Ex6.java

--- revisao-java/src/ex6/Ex6.java
+++ revisao-java/src/ex6/Ex6.java
@@ -1,23 +1,31 @@
 package ex6;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Ex6 {
 
 	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-        double salario[]= new double[10];
-        
-        for(int i=0;i<salario.length;i++) {
-        	System.out.println("Digite o salario "+(i+1)+"=");
-        	salario[i]=sc.nextDouble();
-        }
-       // for(int i=0;i<salario.length;i++) {
-        	System.out.println(Collections.sort(salario));
-        	
-       // }
+		Scanner sc = new Scanner(System.in);
+		List<Double> salario = new ArrayList<>();
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Digite o salario " + (i + 1) + "=");
+			salario.add(sc.nextDouble());
+		}
+		Collections.sort(salario);
+		System.out.println("Salarios em ordem crescente");
+		for (Double sal : salario) {
+			System.out.println(sal);
+		}
+		Collections.reverse(salario);
+		System.out.println("Salarios em ordem decrescente");
+		for (Double sal : salario) {
+			System.out.println(sal);
+		}
+
 	}
 
 }
