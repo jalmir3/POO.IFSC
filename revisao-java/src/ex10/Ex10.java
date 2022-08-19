a79ec55b820faab314cab0bdde3190966cf9a47a
Jalmir Winter <104005909+jalmir3@users.noreply.github.com>
2022-08-18 21:52:03 -0300

    Create Ex10.java

---
+++ revisao-java/src/ex10/Ex10.java
@@ -0,0 +1,16 @@
+package ex10;
+
+import java.util.Scanner;
+
+public class Ex10 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] vetor = { "Amazônia", "Mata Atlântica", "Cerrado", "Caatinga", "Pampa", "Pantanal" };
+
+		vetor[3] = "";// Se caso fosse uma lista,poderia remover o item com lista.remove(3).
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.print(vetor[i] + " ");
+		}
+
+	}
+}
